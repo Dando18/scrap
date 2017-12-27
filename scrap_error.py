d0da583ce82e7b37f83f9ee4aa38e9e47b0f09a0
Daniel Nichols <dando18studios@gmail.com>
2017-12-26 23:53:23 -0500

    colored error and warning messages

--- scrap_error.py
+++ scrap_error.py
@@ -2,12 +2,22 @@
 Error and Warning print protocol
 '''
 
+class bcolors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+
 def print_error(name, message, line_number):
-	print('ERROR (' + str(name) + '): ' + str(message) + ' --- at line #' + str(line_number))
+	print(bcolors.FAIL + 'ERROR (' + str(name) + '): ' + str(message) + ' --- at line #' + str(line_number) + bcolors.ENDC)
 
 
 def print_warning(name, message, line_number):
-	print('WARNING (' + str(name) + '): ' + str(message) + ' --- at line #' + str(line_number))	
+	print(bcolors.WARNING + 'WARNING (' + str(name) + '): ' + str(message) + ' --- at line #' + str(line_number) + bcolors.ENDC)	
 
 
 
