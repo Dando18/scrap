b79dcc865772012b4eccfda1fb45476c3dde01d5
Daniel Nichols <dando18studios@gmail.com>
2017-12-28 11:55:52 -0500

    added kill method to end program

--- scrap_utilities.py
+++ scrap_utilities.py
@@ -2,6 +2,8 @@
 Utility functions for scrap
 '''
 import re
+import sys
+
 
 def is_string(string):
 	return re.match(r'"(.*?)"', string) is not None	
@@ -11,5 +13,7 @@ def is_regex(string):
 	return re.match(r'r"(.*?)"', string) is not None
 
 
+def kill(message):
+	sys.exit(str(message))
 
 
