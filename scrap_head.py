37b6fa536d9152bbb0c87f8e15eb3d3c7e252451
Daniel Nichols <dando18studios@gmail.com>
2017-12-28 15:48:10 -0500

    made script file name the first program argument

--- scrap_head.py
+++ scrap_head.py
@@ -12,19 +12,21 @@ import argparse
 
 # create argument parser and parse arguments
 parser = argparse.ArgumentParser(description='scrap toolbox')
+parser.add_argument('script_file')
 parser.add_argument('--verbose-compile', '--vc', action='store_true', default=False, dest='verbose_compile', help='displays preprocessor and compile time output')
 args = parser.parse_args()
 
 
 # get file name for script
-script_name = 'test_script.scrap'
+script_name = args.script_file
+if script_name[-6:] != '.scrap':
+	script_name = 'script.scrap'
 
 
 # read in script_file
 if args.verbose_compile:
 	print('attempting to read ' + script_name + '...')
-with open(script_name) as script_file:
-	script_text = script_file.read()
+script_text = util.read_from_file(script_name)
 if args.verbose_compile and script_text is not None:
 	print_OK('read ' + script_name + ' successfully!\n', checked=True)
 
