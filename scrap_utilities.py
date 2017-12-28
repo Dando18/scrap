95d9d4b4e5156f52881d87c1b7dd754b51c816a1
Daniel Nichols <dando18studios@gmail.com>
2017-12-28 11:56:31 -0500

    added read and write file utilities

--- scrap_utilities.py
+++ scrap_utilities.py
@@ -1,6 +1,7 @@
 '''
 Utility functions for scrap
 '''
+from scrap_error import *
 import re
 import sys
 
@@ -15,5 +16,26 @@ def is_regex(string):
 
 def kill(message):
 	sys.exit(str(message))
+
+
+def write_to_file(filename, content, append=False, line_number=-1):
+	try:
+		f = open(filename, ('a' if append else 'w'))
+	except IOError:
+		print_error('I/O Error', 'File \'' + filename + '\' could not be opened.', line_number, kill=True)
+
+	with f:
+		f.write(content)
+
+
+def read_from_file(filename, line_number=-1):
+	try:
+		f = open(filename, 'r')
+	except IOError:
+		print_error('I/O Error', 'File \'' + filename + '\' could not be read.', line_number, kill=True)
+	
+	with f:
+		return f.read()
+	
 
 
