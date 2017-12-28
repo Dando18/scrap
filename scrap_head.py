371f6a8338d764c7b4a0d6023ace11cd23fb0087
Daniel Nichols <dando18studios@gmail.com>
2017-12-28 11:53:24 -0500

    added program arguments

--- scrap_head.py
+++ scrap_head.py
@@ -6,6 +6,7 @@ Calls preproccesor and then compile.
 from scrap_preprocessor import Preprocessor
 from scrap_compile import Compile
 from scrap_error import *
+import scrap_utilities as util
 import argparse
 
 
@@ -16,26 +17,40 @@ args = parser.parse_args()
 
 
 # get file name for script
-script_name = 'script.scrap'
+script_name = 'test_script.scrap'
 
 
 # read in script_file
+if args.verbose_compile:
+	print('attempting to read ' + script_name + '...')
 with open(script_name) as script_file:
 	script_text = script_file.read()
+if args.verbose_compile and script_text is not None:
+	print_OK('read ' + script_name + ' successfully!\n', checked=True)
 
 
 # process
+if args.verbose_compile:
+	print('attempting to preprocess source...')
 preprocessor = Preprocessor(script_text)
 processed_text = preprocessor.process()
 
 if args.verbose_compile:
-	print('~~~~~~~~~~~~~~~\nPREPROCESSED TEXT\n~~~~~~~~~~~~~~~\n' + str(script_text))
-	print('~~~~~~~~~~~~~~~\nPROCESSED TEXT\n~~~~~~~~~~~~~~~\n' + str(processed_text) + '\n')
+	print_heading('PREPROCESSED TEXT')
+	print(script_text.strip() + '\n')
+	if processed_text is not None:
+		print_heading('PROCESSED TEXT')
+		print(processed_text + '\n')
+		print_OK('preprocessed source successfully!\n', checked=True)
 
 
 # compile
+if args.verbose_compile:
+	print('attempting to execute source...')
 compiler = Compile(processed_text)
 compiler.compile()
+if args.verbose_compile:
+	print_OK('executed successfully!', checked=True)
 
 
 
