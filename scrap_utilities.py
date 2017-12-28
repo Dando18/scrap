ff0b5a5afbc30f9efa1cb40ca7553136325db8af
Daniel Nichols <dando18studios@gmail.com>
2017-12-28 11:57:27 -0500

    read/write strings to/from variables
    added methods to read and write strings to and from variables of different types

--- scrap_utilities.py
+++ scrap_utilities.py
@@ -1,6 +1,7 @@
 '''
 Utility functions for scrap
 '''
+from scrap_variable import *
 from scrap_error import *
 import re
 import sys
@@ -37,5 +38,36 @@ def read_from_file(filename, line_number=-1):
 	with f:
 		return f.read()
 	
+
+
+'''
+Attempts to get a string from a variable. If type is STRING or NUMERIC, then data is returned
+in string form. If type is FILE, then the file is read into the string. Else 'None' is returned.
+'''
+def read_string_from_variable(var):
+	if var.type_t is Type.STRING:
+		return var.data
+	elif var.type_t is Type.FILE:
+		return read_from_file(var.data)
+	elif var.type_t is Type.NUMERIC:
+		return str(var.data)
+	else:
+		return None
+		
+
+'''
+writes a string into a var. methods pends on its type.
+'''
+def write_string_to_variable(var, string):
+	if var.type_t is Type.STRING:
+		var.data = string
+	elif var.type_t is Type.FILE:
+		write_to_file(var.data, string)
+	elif var.type_t is Type.NUMERIC:
+		var.data = string
+		var.type_t = Type.STRING
+
+
+
 
 
